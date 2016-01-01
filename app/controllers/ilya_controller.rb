563b38659ddfc7419f4a1ea5cb5fa3f10d77d999
khataev <khataev@gmail.com>
2016-01-01 14:12:47 +0300

    date format changed

--- app/controllers/ilya_controller.rb
+++ app/controllers/ilya_controller.rb
@@ -7,10 +7,9 @@ class IlyaController < ApplicationController
 	def index
     init
 
-		@today_date = Date.today
 		@base_usd_rate = usdcur.rate(@base_date) # базовый курс
 		@base_eur_rate = eurcur.rate(@base_date) # базовый курс
-		#puts "base_rate: #{@base_rate}"
+
 		@today_usd_rate = usdcur.rate(nil) # сегодняшний курс
 
 		rel = @today_usd_rate / @base_usd_rate  # отношение курсов
@@ -24,7 +23,13 @@ class IlyaController < ApplicationController
 	private 
 
   def init
+    date_format = "%d.%m.%Y"
     @base_date = Date.new(2014,8,1) 
+
+    @today_date = Date.today
+    @today_date_str = @today_date.strftime(date_format)
+
+    @base_date_str = @base_date.strftime(date_format)
     @base_debt = 2200000
     @base_debt_str = separate_space(@base_debt)
     @currencies = Currency.all
