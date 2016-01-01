b97865c288c64db70e4403bb872f06d40cc32e8d
khataev <khataev@gmail.com>
2016-01-01 13:46:52 +0300

    some optimization, visual refactoring and euro added

--- app/controllers/ilya_controller.rb
+++ app/controllers/ilya_controller.rb
@@ -1,34 +1,41 @@
 require 'date'
-
 class IlyaController < ApplicationController
+	
 	@@usd_id = 'R01235'
-
-	#@base_date = Date.new(2014,8,1)	
-	#@base_debt = 2200000
-	@currency
-
-	def initialize
-		@base_date = Date.new(2014,8,1)	
-		@base_debt = 2200000
-		@base_debt_str = separate_space(@base_debt)
-	end
+	@@eur_id = 'R01239'
 
 	def index
+    init
 
-		@base_rate = usdcur().rate(@base_date) # базовый курс
-		puts "base_rate: #{@base_rate}"
-		@today_rate = usdcur().rate(nil) # сегодняшний курс
+		@today_date = Date.today
+		@base_usd_rate = usdcur.rate(@base_date) # базовый курс
+		@base_eur_rate = eurcur.rate(@base_date) # базовый курс
+		#puts "base_rate: #{@base_rate}"
+		@today_usd_rate = usdcur.rate(nil) # сегодняшний курс
 
-		rel = @today_rate / @base_rate  # отношение курсов
+		rel = @today_usd_rate / @base_usd_rate  # отношение курсов
 		@cur_debt_str = separate_space((@base_debt * rel).round(0))
+
+		@usd = (@base_debt / @base_usd_rate).round(0)
+		@eur = (@base_debt / @base_eur_rate).round(0)
 
 	end
 
 	private 
 
+  def init
+    @base_date = Date.new(2014,8,1) 
+    @base_debt = 2200000
+    @base_debt_str = separate_space(@base_debt)
+    @currencies = Currency.all
+  end
+
 	def usdcur
-		currencies = Currency.all
-		@currency ||= currencies.select {|c| c.id == @@usd_id}.first
+		@usd_currency = @currencies.select {|c| c.id == @@usd_id}.first
+	end
+
+	def eurcur
+		@eur_currency = @currencies.select {|c| c.id == @@eur_id}.first
 	end
 
 	def separate_space(number)
