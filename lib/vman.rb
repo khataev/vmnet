479f8c1d010ac5694d7e84c10d8b19f65788abcc
Andrey Khataev <khataev@gmail.com>
2016-10-06 16:07:53 +0300

    Add check_ping_guest method for lost network reset in guest VM

--- lib/vman.rb
+++ lib/vman.rb
@@ -1,9 +1,31 @@
+require 'net/ping'
+
 class Vman
-	def off
-		system('VBoxManage controlvm Win7  setlinkstate1 off')
+
+  def off
+    Vman.off
+  end
+
+  def on
+    Vman.on
+  end
+
+	def self.off
+		system('sudo -u khataev VBoxManage controlvm Win7  setlinkstate1 off')
 	end
 
-	def on
-		system('VBoxManage controlvm Win7  setlinkstate1 on')
-	end
+	def self.on
+		system('sudo -u khataev VBoxManage controlvm Win7  setlinkstate1 on')
+  end
+
+  def self.check_ping_guest
+    @icmp = Net::Ping::ICMP.new('192.168.1.199')
+
+    unless @icmp.ping
+      puts "unless ping: #{@icmp.ping}"
+      self.off
+      sleep(10)
+      self.on
+    end
+  end
 end
