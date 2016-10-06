4d6b14891e6a2796dba153523e8bb9af3195315c
Andrey Khataev <khataev@gmail.com>
2016-10-06 16:09:04 +0300

    Play with web_console options

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Vmnet
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.web_console.whitelisted_ips = '192.168.1.100'
+    # config.web_console.development_only = false
   end
 end
