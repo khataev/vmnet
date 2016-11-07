2eaa75d91d5232eaf0946914ba05e3155d024a68
Andrey Khataev <khataev@gmail.com>
2016-11-07 20:53:22 +0300

    Change IP whitelist

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.web_console.whitelisted_ips = %w( 192.168.1.100 46.188.49.130)
+  config.web_console.whitelisted_ips = %w( 192.168.1.100 46.188.49.130 10.124.216.22 )
   # config.web_console.development_only = false
 end
