adf67b4e1655bec11f396adebd8ea9512ff9c19a
Andrey Khataev <khataev@gmail.com>
2016-11-08 20:41:08 +0300

    Refactor and beautify currencies and rates loading

--- app/controllers/api/v1/rates_controller.rb
+++ app/controllers/api/v1/rates_controller.rb
@@ -1,50 +1,53 @@
 require 'date'
 
 class Api::V1::RatesController < ApplicationController
-	protect_from_forgery except: :usd
-	respond_to :json
+  protect_from_forgery except: :usd
+  respond_to :json
 
-	before_action :init
+  before_action :init, only: :usd
 
-	@@usd_id = 'R01235'
-	@@eur_id = 'R01239'
-	@@date_format = '%Y%m%d'
+  @@usd_id = 'R01235'
+  @@eur_id = 'R01239'
+  @@date_format = '%Y%m%d'
 
-	def usd
-		ans = {
-			rates: {}
-		}
-		if @start_date && @end_date
-			ans.merge!({start_date: @start_date, end_date: @end_date})
-			@start_date.upto(@end_date) do |day|
-				ans[:rates][day.to_s] = usdcur.rate(day)
-			end
-		else
-			ans[:error] = "Error parsing start or end date"
-		end
+  def usd
+    ans = {
+        rates: {}
+    }
+    if @start_date && @end_date
+      ans.merge!({start_date: @start_date, end_date: @end_date})
+      # TODO: Refactor with mass select at once instead of multiple selects
+      @start_date.upto(@end_date) { |day| ans[:rates][day.to_s] = usdcur.rate(day) } if usdcur
+    else
+      ans[:error] = "Error parsing start or end date"
+    end
 
-		ans[:error] = "Error geting data from CBR service" unless @currencies
+    ans[:error] = "Error geting data from CBR service" unless @currencies || usdcur
 
-		# ans = { rate: usdcur.rate(nil), start_date: @start_date, end_date: @end_date }
-		respond_with ans, callback: params[:callback]
-	end
+    # ans = { rate: usdcur.rate(nil), start_date: @start_date, end_date: @end_date }
+    respond_with ans, callback: params[:callback]
+  end
 
-	private
+  private
 
-	def init
-		begin
-			@currencies = Currency.all
-			@start_date = Date.strptime(params[:start_date],@@date_format) if params[:start_date]
-			@end_date = Date.strptime(params[:end_date],@@date_format) if params[:end_date]
-		rescue
-		end
-	end
+  def init
 
-	def usdcur
-		@usd_currency = @currencies.select {|c| c.id == @@usd_id}.first
-	end
+    begin
+      @start_date = Date.strptime(params[:start_date], @@date_format) if params[:start_date]
+      @end_date = Date.strptime(params[:end_date], @@date_format) if params[:end_date]
+    rescue
+    end
+  end
 
-	def eurcur
-		@eur_currency = @currencies.select {|c| c.id == @@eur_id}.first
-	end
+  def currencies
+    @currencies ||= Currency.all
+  end
+
+  def usdcur
+    @usd_currency ||= currencies.select { |c| c.id == @@usd_id }.first if currencies
+  end
+
+  def eurcur
+    @eur_currency ||= currencies.select { |c| c.id == @@eur_id }.first if currencies
+  end
 end
--- lib/currency.rb
+++ lib/currency.rb
@@ -4,73 +4,72 @@ require 'date'
 
 # Оболочка над API ЦБ РФ: http://www.cbr.ru/scripts/Root.asp?PrtId=SXML
 class Currency
-	@@baseurl = "http://www.cbr.ru/scripts/XML_val.asp"
-	@@query   = URI.encode_www_form("d" => "0")
-	@@address = URI("#@@baseurl?#@@query")
-	@@list = nil
-	@@rates = nil
-	
+  @@list, @@rates = nil, nil
 
-	def self.all
-		load if @@list.nil?
-		@@list
-	end
+  # Returns entire currencies list
+  def self.all
+    begin
+      load if @@list.nil?
+      Rails.logger.debug 'Currencies list loaded correctly'
+    rescue
+      Rails.logger.debug 'Currencies list load error'
+    end
+    @@list
+  end
 
-	def self.load
-		@@list = []
-		@@rates = {}
-		doc = Nokogiri::XML(open(@@address)) #{|f| p f.content_type, f.charset, f.read.encoding }
-		doc.encoding = "UTF-8"
-		doc.xpath("//Valuta/Item").each { |item|
-			c = Currency.new
-			c.id = item.xpath("@ID").text
-			c.name = item.xpath("Name/text()")
-			c.engname = item.xpath("EngName/text()")
-			c.nominal = item.xpath("Nominal/text()")
-			@@list << c
-		}
-		#@@list.each { |c|
-		#	puts "#{c.id}, #{c.name}, #{c.engname}, #{c.nominal}"
-		#}
-	end
+  # Load Currencies list
+  def self.load
+    @@list = []
+    @@rates = {}
+    baseurl = "http://www.cbr.ru/scripts/XML_val.asp"
+    query = URI.encode_www_form("d" => "0")
+    address = URI("#{baseurl}?#{query}")
+    doc = Nokogiri::XML(open(address)) #{|f| p f.content_type, f.charset, f.read.encoding }
+    doc.encoding = "UTF-8"
+    doc.xpath("//Valuta/Item").each do |item|
+      c = Currency.new
+      c.id = item.xpath("@ID").text
+      c.name = item.xpath("Name/text()")
+      c.engname = item.xpath("EngName/text()")
+      c.nominal = item.xpath("Nominal/text()")
+      @@list << c
+    end
+  end
 
-	def rate_req(date)		
-		baseurl = 'http://www.cbr.ru/scripts/XML_daily.asp' << (date.nil? ? '' : date.strftime('?date_req=%d/%m/%Y') )
-		address = URI("#{baseurl}")
+  # Load currency rate on date
+  def rate_req(date)
+    begin
+      baseurl = 'http://www.cbr.ru/scripts/XML_daily.asp' << (date.nil? ? '' : date.strftime('?date_req=%d/%m/%Y'))
+      address = URI("#{baseurl}")
 
-		doc = Nokogiri::XML(open(address))
+      doc = Nokogiri::XML(open(address))
 
-		doc.encoding = "UTF-8"
-		#doc.xpath("//Valuta/Item").each{ |item|
-		#	a = item.xpath("@ID").text
-		#	puts a.inspect
-		#	res = 0
-		#	res = 1 if a == @id.to_s
-		#	puts "'#{a}', '#@id', '#{res}'"
-		#}
-		doc
-	end
+      doc.encoding = "UTF-8"
+      doc
+    rescue
+      Rails.logger.debug "Currency #{:id} load rate on date #{date} error"
+    end
+  end
 
-	def rate(date)
-		date = date.nil? ? DateTime.now.to_date : date
+  def rate(date)
+    date = date.nil? ? DateTime.now.to_date : date
     key = date.to_s.to_sym
-		if @@rates.has_key? key 
-			doc = @@rates[key]
+    if @@rates.has_key? key
+      doc = @@rates[key]
     elsif row = RatesHistory.find_by_date(date)
       doc = Nokogiri::XML(row.rates)
       @@rates[key] = doc
-		else
-			doc = rate_req(date)
-			@@rates[key] = doc
-      RatesHistory.create!(date: date, rates: doc)
-		end	
+    else
+      doc = rate_req(date)
+      doc and @@rates[key] = doc and RatesHistory.create!(date: date, rates: doc)
+    end
 
-		rate_node = doc.xpath("//ValCurs/Valute").select { |item| item.xpath("@ID").text == @id }.first
-		#puts rate_node
-		(rate_node.search('Value').text.sub(',','.').to_f / rate_node.search('Nominal').text.to_f).round(4) unless rate_node.nil?
+    rate_node = doc.xpath("//ValCurs/Valute").select { |item| item.xpath("@ID").text == @id }.first
+    #puts rate_node
+    (rate_node.search('Value').text.sub(',', '.').to_f / rate_node.search('Nominal').text.to_f).round(4) unless rate_node.nil?
 
-	end
+  end
 
-	attr_accessor :id, :name, :engname, :nominal
+  attr_accessor :id, :name, :engname, :nominal
 
 end
